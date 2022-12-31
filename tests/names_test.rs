90b0250b9e4afac8b34d7f474a65bcf499e55db2
Orlyn Geraño <orlyn_gerano@yahoo.com>
2022-12-31 21:48:22 +0100

    Add test for names

---
+++ tests/names_test.rs
@@ -0,0 +1,15 @@
+use blackjack::game::{player::Player, blackjack::{DEFAULT_SOFTPOINTS,BlackJack}};
+
+#[test]
+fn names(){
+    let player_name = String::from("Bob");
+    let dealer_name = String::from("Alice");
+
+    let black_jack = BlackJack::new(Player::new(player_name.clone()),Player::new(dealer_name.clone()), DEFAULT_SOFTPOINTS);
+    
+    let game_player_name = black_jack.get_player().get_name();
+    let game_dealer_name = black_jack.get_dealer().get_name();
+
+    assert!(game_player_name.eq(&player_name),  "game_player_name={} player_name={}", game_player_name, player_name);
+    assert!(game_dealer_name.eq(&dealer_name),  "game_dealer_name={} dealer_name={}", game_dealer_name, dealer_name);
+}   
