767a2dfaa00a145730b2f1c8cea99552b8fed777
orlyn <orlyn_gerano@yahoo.com>
2026-02-15 17:13:37 +0100

    Improvements

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,20 @@ use clap::Parser;
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Name of the dealer
-    #[arg(short, long, default_value_t = String::from("Dealer"))]
+    #[arg(short, long, default_value = "Dealer")]
     dealer: String,
 
     /// Name of the player
-    #[arg(short, long, default_value_t =  String::from("Player"))]
+    #[arg(short, long, default_value = "Player")]
     player: String,
 
-    /// Softpoints
+    /// Soft points
     #[arg(short, long, default_value_t = game::blackjack::DEFAULT_SOFTPOINTS)]
-    softpoints: u8,
+    soft_points: u8,
 }
 
 fn main() {
     let args = Args::parse();
-    let mut black_jack_console = BlackJackConsole::new(args.player, args.dealer, args.softpoints);
+    let mut black_jack_console = BlackJackConsole::new(args.player, args.dealer, args.soft_points);
     black_jack_console.play();
 }
